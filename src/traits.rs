b013ca5056f6d10decda5dbf05f142bb4406c7fb
davelively14 <dave.lively@salesloft.com>
2019-07-31 18:00:13 -0400

    Finished 27

--- src/traits.rs
+++ src/traits.rs
@@ -1,3 +1,13 @@
+struct Visa {
+    verification: u32,
+}
+
+impl CreditCharge for Visa {
+    fn charge_with_id(&self, id: u32) -> bool {
+        id % 3 == self.verification % 4
+    }
+}
+
 struct BitCredit {
     btcnumber: u32,
 }
@@ -12,13 +22,20 @@ trait CreditCharge {
     fn charge_with_id(&self, id: u32) -> bool;
 }
 
-pub fn run_examples() {
-    let card = BitCredit { btcnumber: 1024 };
-    let code = 4096;
-
-    if card.charge_with_id(code) {
+fn transact<Q: CreditCharge>(card: Q) {
+    // get verif code
+    let id = 4096;
+    if card.charge_with_id(id) {
         println!("Success!!");
     } else {
-        println!("Failure!!");
+        panic!("Invalid code!");
     }
+}
+
+pub fn run_examples() {
+    let bit_credit = BitCredit { btcnumber: 1024 };
+    let visa = Visa { verification: 1024 };
+
+    transact(bit_credit);
+    transact(visa);
 }
